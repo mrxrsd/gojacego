eeb0015992ce1f9855ed5d7a1721101e0239d200
mrxrsd <mrxrsd@gmail.com>
2020-08-29 16:05:30 -0300

    TokenReader

--- TokenType.go
+++ TokenType.go
@@ -4,3 +4,13 @@ package gojacego
 	The type of the token.
 */
 type TokenType int
+
+const (
+	INTEGER = iota
+	FLOATING_POINT
+	TEXT
+	OPERATION
+	LEFT_BRACKET
+	RIGHT_BRACKET
+	ARGUMENT_SEPARATOR
+)
