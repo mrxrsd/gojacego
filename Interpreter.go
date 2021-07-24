0049bc9105ade8b2706fce0f8327cd66b22e2ff0
mrxrsd <mrxrsd@gmail.com>
2021-07-23 23:50:28 -0300

    Interpreter almost finish

--- Interpreter.go
+++ Interpreter.go
@@ -2,6 +2,8 @@ package gojacego
 
 import (
 	"errors"
+	"fmt"
+	"math"
 )
 
 type Interpreter struct {
@@ -39,12 +41,102 @@ func execute(op Operation, vars FormulaVariables) (float64, error) {
 		right, _ := execute(cop.OperationTwo, vars)
 
 		return left * right, nil
+	} else if cop, ok := op.(*AddOperation); ok {
+		left, _ := execute(cop.OperationOne, vars)
+		right, _ := execute(cop.OperationTwo, vars)
+
+		return left + right, nil
 	} else if cop, ok := op.(*SubtractionOperation); ok {
 		left, _ := execute(cop.OperationOne, vars)
 		right, _ := execute(cop.OperationTwo, vars)
 
 		return left - right, nil
+	} else if cop, ok := op.(*DivisorOperation); ok {
+		left, _ := execute(cop.Dividend, vars)
+		right, _ := execute(cop.Divisor, vars)
+
+		return left / right, nil
+	} else if cop, ok := op.(*ModuloOperation); ok {
+		left, _ := execute(cop.Dividend, vars)
+		right, _ := execute(cop.Divisor, vars)
+
+		return math.Mod(left, right), nil
+	} else if cop, ok := op.(*ExponentiationOperation); ok {
+		left, _ := execute(cop.Base, vars)
+		right, _ := execute(cop.Exponent, vars)
+
+		return math.Pow(left, right), nil
+	} else if cop, ok := op.(*UnaryMinusOperation); ok {
+		arg, _ := execute(cop.Operation, vars)
+		return -arg, nil
+	} else if cop, ok := op.(*AndOperation); ok {
+		left, _ := execute(cop.OperationOne, vars)
+		right, _ := execute(cop.OperationTwo, vars)
+
+		if left != 0 && right != 0 {
+			return 1.0, nil
+		}
+		return 0.0, nil
+	} else if cop, ok := op.(*OrOperation); ok {
+		left, _ := execute(cop.OperationOne, vars)
+		right, _ := execute(cop.OperationTwo, vars)
+
+		if left != 0 || right != 0 {
+			return 1.0, nil
+		}
+		return 0.0, nil
+	} else if cop, ok := op.(*LessThanOperation); ok {
+		left, _ := execute(cop.OperationOne, vars)
+		right, _ := execute(cop.OperationTwo, vars)
+
+		if left < right {
+			return 1.0, nil
+		}
+		return 0.0, nil
+	} else if cop, ok := op.(*LessOrEqualThanOperation); ok {
+		left, _ := execute(cop.OperationOne, vars)
+		right, _ := execute(cop.OperationTwo, vars)
+
+		if left <= right {
+			return 1.0, nil
+		}
+		return 0.0, nil
+	} else if cop, ok := op.(*GreaterThanOperation); ok {
+		left, _ := execute(cop.OperationOne, vars)
+		right, _ := execute(cop.OperationTwo, vars)
+
+		if left > right {
+			return 1.0, nil
+		}
+		return 0.0, nil
+	} else if cop, ok := op.(*GreaterOrEqualThanOperation); ok {
+		left, _ := execute(cop.OperationOne, vars)
+		right, _ := execute(cop.OperationTwo, vars)
+
+		if left >= right {
+			return 1.0, nil
+		}
+		return 0.0, nil
+	} else if cop, ok := op.(*EqualOperation); ok {
+		left, _ := execute(cop.OperationOne, vars)
+		right, _ := execute(cop.OperationTwo, vars)
+
+		if left == right {
+			return 1.0, nil
+		}
+		return 0.0, nil
+	} else if cop, ok := op.(*NotEqualOperation); ok {
+		left, _ := execute(cop.OperationOne, vars)
+		right, _ := execute(cop.OperationTwo, vars)
+
+		if left != right {
+			return 1.0, nil
+		}
+		return 0.0, nil
+	} else if cop, ok := op.(*FunctionOperation); ok {
+
+		return 0.0, fmt.Errorf("Not implemented %T", cop)
 	}
 
-	return 1.0, nil
+	return 0.0, fmt.Errorf("Not implemented %T", op)
 }
--- Interpreter_test.go
+++ Interpreter_test.go
@@ -15,3 +15,22 @@ func TestBasicInterpreterSubstraction(test *testing.T) {
 		test.Errorf("Expected: -3.0, got: %f", ret)
 	}
 }
+
+func TestBasicInterpreter1(test *testing.T) {
+	interpreter := &Interpreter{}
+
+	// 6 + (2 * 4)
+
+	ret, _ := interpreter.Execute(
+		NewAddOperation(
+			Integer,
+			NewConstantOperation(Integer, 6),
+			NewMultiplicationOperation(
+				Integer,
+				NewConstantOperation(Integer, 2),
+				NewConstantOperation(Integer, 4))), nil)
+
+	if ret != 14.0 {
+		test.Errorf("Expected: 14.0, got: %f", ret)
+	}
+}
